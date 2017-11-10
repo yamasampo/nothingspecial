9074e8eb499e9600d514f5a64167eb89242edf37
Haruka Yamashita <haruka@tests-Mac-Pro-6.local>
2017-11-10 09:27:00 +0900

    added get_fasta, read_big_pickle and write_big_pickle.

--- biostr.py
+++ biostr.py
@@ -2,6 +2,49 @@ import os
 import sys
 import re
 import pandas as pd
+import pickle
+
+def get_fasta_df(fasta_path):
+    ## dataframe for mfa database
+    # get paths
+    dname = os.path.basename(os.path.dirname(fasta_path))
+    fname = os.path.basename(fasta_path)
+
+    # collect data
+    seq_name = ''
+    seq=''
+    dname_list = []
+    fname_list = []
+    seqname_list = []
+    seq_list = []
+
+    with open(fasta_path, 'r') as f:
+        aligned = 0
+
+        for line in f.readlines():
+
+            if line.startswith('>'):
+                seq_name = line[1:-1]
+
+            else:
+                seq = line.rstrip()
+
+                dname_list.append(dname)
+                fname_list.append(fname)
+                seqname_list.append(seq_name)
+                seq_list.append(seq)
+
+    # create dataframe
+    fasta_df = pd.DataFrame({'dir_name': dname_list,
+                           'file_name': fname_list,
+                           'seqname': seqname_list,
+                           'seq': seq_list
+                           })
+    # rearrange columns
+    return fasta_df.ix[:,['dir_name',
+                          'file_name',
+                          'seqname',
+                          'seq']]
 
 def to_filelist(dir_path):
 
@@ -40,3 +83,27 @@ def get_file_list(path, avoid='itemnum', prefix=None, sufix=None):
     print('{0}: # files is {1}'.format(os.path.basename(os.path.dirname(path)), len(flist)))
 
     return flist
+
+def read_big_pickle(pickle_path):
+
+    bytes_in = bytearray(0)
+    max_bytes = 2**31 - 1
+    input_size = os.path.getsize(pickle_path)
+
+    with open(pickle_path, 'rb') as f_in:
+
+        for _ in range(0, input_size, max_bytes):
+            bytes_in += f_in.read(max_bytes)
+
+    return pickle.loads(bytes_in)
+
+def write_big_pickle(pickle, out_path):
+
+    n_bytes = sys.getsizeof(pickle)
+    max_bytes = 2**31 - 1
+    bytes_out = pickle.dumps(pickle)
+
+    with open(out_path, 'wb') as f_out:
+
+        for idx in range(0, n_bytes, max_bytes):
+            f_out.write(bytes_out[idx:idx+max_bytes])
