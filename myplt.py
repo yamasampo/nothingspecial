30e022ed667b0f2c75b5dd37c17cbcc8db03b6aa
Haruka Yamashita <>
2017-11-24 21:19:20 +0900

    commit for now. please change to be able to set xticklabels even when comparing by different mutations like 'TC' and 'CT' in compare_means_bar() function.

--- myplt.py
+++ myplt.py
@@ -4,9 +4,9 @@ import pandas as pd
 import numpy as np
 
 def get_means_CIs(data_df, rep_num, category_column, data_column, rep_column='rep',
-                  xlabel=None, ylabel=None, label=None, categories=None, style=None,
-                  ci=95, width=0.3, space=0.5, alpha=1, color='#e41a1c', error_kw={'ecolor': '0.3'},
-                  show_fig=False, save_fig=False, fig_fname=None, return_fmt='array'):
+                  xlabel=None, ylabel=None, label=None, categories=None, style=None, xticklabels=None,
+                  ci=95, width=0.3, space=0.5, alpha=1, color='#e41a1c', error_kw={'ecolor': '0.3', 'elinewidth':'1'},
+                  show_fig=False, save_fig=False, fig_fname=None, return_fmt='array', **kwargs):
     '''
     values that will be plotted should be contained
     as being specified by category and rep values in data_df
@@ -15,8 +15,10 @@ def get_means_CIs(data_df, rep_num, category_column, data_column, rep_column='re
     if not categories:
         categories = set(data_df[category_column].tolist())
     data = data_df.drop_duplicates([category_column, rep_column, data_column])
+    print(len(data.index))
+    print(categories)
     if len(data.index) != len(categories)*rep_num:
-        raise Exception('Given data is not unique by given category and replication columns.')
+        raise Exception('Given data is not unique by given category and replication columns. Input data length is {0} inspite expected length is {1}.'.format(len(data.index), len(categories)*rep_num))
 
     ## Get mean and CI
     means_list = []
@@ -30,6 +32,7 @@ def get_means_CIs(data_df, rep_num, category_column, data_column, rep_column='re
         rep_list = list(set(data[data[category_column] == c]['rep'].tolist()))
         rep_list.sort()
         if exp_rep_list != rep_list:
+            print(rep_list)
             raise Exception('replication error')
 
         v = data[data[category_column] == c][data_column]
@@ -44,50 +47,55 @@ def get_means_CIs(data_df, rep_num, category_column, data_column, rep_column='re
     upper_yerr = upper_ci_array - means_array
     yerrs = [lower_yerr, upper_yerr]
 
+    df = pd.DataFrame({'mean': means_array, 'lower_ci': lower_ci_array, 'upper_ci': upper_ci_array})
+
     if show_fig or save_fig:
 
         if style:
             plt.style.use(style)
         fig, ax = plt.subplots()
         d = space + width
-        index = np.arange(1, len(means_array)*d+1, d)
+        index = np.linspace(1, 1+d*len(means_array), len(means_array),endpoint=True)
 
         ax.bar(index, means_array, width=width, alpha=alpha, color=color, yerr=yerrs,
                error_kw=error_kw, label=label)
+
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
         ax.set_xticks(index)
-        ax.set_xticklabels(categories)
+        if xticklabels:
+            ax.set_xticklabels(xticklabels)
+        else:
+            ax.set_xticklabels(categories)
         ax.legend()
 
         fig.tight_layout()
         if save_fig:
             plt.savefig(fig_fname, format = 'png', dpi=300)
-            df.to_csv(fig_fname)
+            df.to_csv(os.path.join(os.path.dirname(fig_fname), os.path.basename(fig_fname).split('.')[0]+'.csv'), index=False)
 
         plt.show()
 
     if return_fmt == 'array':
         return  [means_array, yerrs]
     elif return_fmt == 'df':
-        return pd.DataFrame({'mean': means_array, 'lower_ci': lower_ci_array, 'upper_ci': upper_ci_array})
+        return df
 
 def compare_means_bar(data_df, rep_num, category_column, data_column, compare,
-                      xlabel, ylabel, colors=[], rep_column='rep', categories=[],
+                      xlabel, ylabel, colors=[], rep_column='rep', categories_list=[], xticklabels=None,
                       ci=95, width=0.3, space=0.5, alpha=1, style=None, title=None, label=[],
                       tfontsize=18, xlfontsize=15, ylfontsize=15, lfontsize=10, yfontsize=9,
-                      error_kw={'ecolor': '0.3'}, save_fig=False, fig_fname=None):
+                      error_kw={'ecolor': '0.3', 'elinewidth':'1'}, save_fig=False, fig_fname=None):
     '''
     data that will be compared should be specified by rep and category(xlabel),
     but except compare category (label) and bins(collection of comparings).
 
     '''
 
-    if not categories:
-        categories = list(set(data_df[category_column].tolist()))
-        categories.sort()
+    data_df.reset_index(inplace=True, drop=True)
     color_list = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf']
     summary_df = pd.DataFrame(index=[], columns=[])
+    column_list = []
 
     for k, v in compare.items():
 
@@ -104,56 +112,68 @@ def compare_means_bar(data_df, rep_num, category_column, data_column, compare,
             v = list(set(data_df[k].tolist()))
             v.sort()
 
+        # for each items to be compared
         for i, a in enumerate(v):
 
             print(k, a)
+            # specify a series of one of compared data
             data = data_df.loc[data_df[data_df[k] == a].index]
-
             means_array = []
             yerrs = []
+
             means_array, yerrs = get_means_CIs(data_df=data, rep_num=rep_num,
                                                category_column=category_column, return_fmt='array',
-                                               data_column=data_column, categories=categories)
-
+                                               data_column=data_column, categories=categories_list)
             print('mean array:', means_array)
             print('yerr arrays:',yerrs)
 
             d = space + len(v)*width
-            index = np.arange(1, len(means_array)*d+1, d)
+            index = np.linspace(1, 1+d*len(means_array), len(means_array),endpoint=True)
+            print(len(index))
             x = index + width*i
             if label:
                 l = label[i]
             else:
                 l = a
 
+            # make bar plot
             ax.bar(x, means_array, width=width, alpha=alpha, color=colors[i],
                    yerr=yerrs, error_kw=error_kw, label=l)
+            # set xticklabels
+            ax.set_xticks(index+(width/2)*(len(v)-1))
+            if xticklabels:
+                ax.set_xticklabels(xticklabels)
+            else:
+                ax.set_xticklabels(categories_list)
             # make summary DataFrame
             lower_yerr = yerrs[0]
             upper_yerr = yerrs[1]
             lower_ci = means_array - lower_yerr
             upper_ci = means_array + upper_yerr
 
-            if len(summary_df.index) == 0:
-                summary_df = pd.DataFrame({'{}_mean'.format(l): means_array, '{}_lower_ci'.format(l): lower_ci, '{}_upper_ci'.format(l): upper_ci})
-
-            else:
-                summary_df['{}_mean'.format(l)] = means_array
-                summary_df['{}_lower_ci'.format(l)] = lower_ci
-                summary_df['{}_upper_ci'.format(l)] = upper_ci
+            # if len(summary_df.index) == 0:
+            #     summary_df = pd.DataFrame({'{}_mean'.format(l): means_array, '{}_lower_ci'.format(l): lower_ci, '{}_upper_ci'.format(l): upper_ci})
+            summary_df['{}_mean'.format(l)] = means_array
+            summary_df['{}_lower_ci'.format(l)] = lower_ci
+            summary_df['{}_upper_ci'.format(l)] = upper_ci
+            column_list.append('{}_mean'.format(l))
+            column_list.append('{}_lower_ci'.format(l))
+            column_list.append('{}_upper_ci'.format(l))
 
         if title:
             ax.set_title(title, fontsize=tfontsize)
         ax.set_xlabel(xlabel, fontsize=xlfontsize)
         ax.set_ylabel(ylabel, fontsize=ylfontsize)
-        ax.set_xticks(index+(width/2)*(len(v)-1))
-        ax.set_xticklabels(categories)
+
         ax.tick_params(labelsize=yfontsize)
         ax.legend(fontsize=lfontsize)
 
         fig.tight_layout()
+
+        summary_df = summary_df.ix[:, column_list]
         if save_fig:
             plt.savefig(fig_fname, format = 'png', dpi=300)
+            summary_df.to_csv(os.path.join(os.path.dirname(fig_fname), os.path.basename(fig_fname).split('.')[0]+'.csv'), index=False)
 
         plt.show()
 
@@ -163,7 +183,7 @@ def compare_means_bar_loop(data_df, rep_num, category_column, data_column, compa
                       xlabel, ylabel, colors=[], rep_column='rep', categories=[],
                       ci=95, width=0.3, space=0.5, alpha=1, title=None, label=[], style=None,
                       tfontsize=18, xlfontsize=15, ylfontsize=15, lfontsize=10, yfontsize=9,
-                      error_kw={'ecolor': '0.3'}, save_fig=False, fig_fname_pre=None):
+                      error_kw={'ecolor': '0.3', 'elinewidth':'1'}, save_fig=False, fig_fname_pre=None):
     '''
     Compare means of data that is specified by category and replication columns and
     loop this calculation by data in given series.
