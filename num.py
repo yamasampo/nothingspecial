aed269f0b25f5f3a5c83bdfe3d96e72b288a106e
Haruka Yamashita <>
2018-03-15 20:54:53 +0900

    added bootstrap() function. v.1.4.

--- num.py
+++ num.py
@@ -2,9 +2,10 @@ import os
 import re
 import pandas as pd
 from collections import Counter
+import numpy as np
 
-__version__='1.3'
-__update__='180223'
+__version__='1.4'
+__update__='180315'
 __author__='Haruka Yamashita'
 
 def slide_window(data_df, win, col_list):
@@ -66,7 +67,7 @@ def count_occurrence(df, column, out_path=None, by=None, to_file=True, **kwargs)
         print('saved to {}'.format(out_path))
         return res_df
 
-def search(df, **kwargs):
+def search_items_df(df, **kwargs):
     '''
     Search rows which have specifies items from a given dataframe.
     Please pass key words for searching to **kwargs.
@@ -119,12 +120,78 @@ def search(df, **kwargs):
     for k,v in kwargs.items():
         if isinstance(v, tuple):
             for i in v:
-                print('column "{0}" filtered to be "{1}".'.format(k, i))
+                print('search items in "{0}" column mathing to "{1}"'.format(k, v))
                 res_df = f(res_df, k, i)
         elif isinstance(v, str):
-            print('column "{0}" filtered to be "{1}".'.format(k, v))
+            print('search items in "{0}" column mathing to "{1}"'.format(k, v))
             res_df = f(res_df, k, v)
         elif isinstance(v, int):
-            print('column "{0}" filtered to be "{1}".'.format(k, v))
+            print('search items in "{0}" column mathing to "{1}"'.format(k, v))
             res_df = res_df[res_df[k] == v]
     return res_df
+
+def bootstrap(data, rep_num):
+    '''
+    Returns a list of means of resampled data.
+    '''
+    boot_means = []
+    for i in range(rep_num):
+        index = np.random.choice(len(data), len(data), replace=True)
+        new_data = data[index]
+
+        boot_means.append(np.mean(new_data))
+
+    if len(boot_means) != rep_num:
+        raise Exception('bootstrapped data has excess or lack of data.')
+
+    boot_array = np.array(boot_means)
+    boot_array.sort()
+    return boot_array
+
+def median_CIs(data, ci=95):
+    lower_ci = np.percentile(data, 100-ci)
+    median = np.median(data)
+    upper_ci = np.percentile(data, ci)
+
+    return lower_ci, median, upper_ci
+
+def bootstrap_df(df, rep_num, ci=95):
+    '''
+    Returns df containing median and confidence intervals.
+    Parameters
+    ----------
+        df: DataFrame (pandas)
+            rows of df should be composed of data of each sample and columns should be categories.
+        rep_num: int
+            number of replicates. bootstrap will be performed rep_num times.
+        ci: int (0<= ci <= 100)
+            percentile for confidence intervals.
+
+    Returns
+    -------
+        stat_df: DataFrame (pandas)
+            stat_df is composed of a table containing arrays of median, lower ci and upper ci.
+    '''
+    # drop NaN from df
+    df2 = df.dropna(how='any')
+
+    cnt = 0
+    out_list=[]
+    print('Bootstrap', end=' ', flush=True)
+    for i, row in df2.iteritems():
+        # extract data for each category
+        data = np.array(row)
+
+        # resample data
+        boot_data = bootstrap(data, rep_num)
+
+        # compute median and CI
+        out_list.append(median_CIs(boot_data, ci))
+        cnt += 1
+        if cnt % 100 == 0:
+            print('.', end='', flush=True)
+
+    print(' Done.', flush=True)
+
+    stat_df = pd.DataFrame(out_list, columns=['ci_{}'.format(100-ci), 'median', 'ci_{}'.format(ci)])
+    return stat_df
