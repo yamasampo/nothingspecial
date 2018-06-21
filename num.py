a841f4561de45b139b8f803966389a7158439585
Haruka Yamashita <>
2018-06-21 12:18:00 +0900

    moved search_items_df() to variables.DB class. v.2.4

--- num.py
+++ num.py
@@ -4,8 +4,8 @@ import pandas as pd
 from collections import Counter
 import numpy as np
 
-__version__ = '2.3'
-__updated__ = '180529'
+__version__ = '2.4'
+__updated__ = '180621'
 __author__ = 'Haruka Yamashita'
 
 def slide_window(data_df, win_size, dat_col_list):
@@ -71,67 +71,6 @@ def count_occurrence(df, column, out_path=None, by=None, to_file=True, **kwargs)
         res_df.sort_values(by=c, inplace=True).to_csv(out_path, index=False)
         print('saved to {}'.format(out_path))
         return res_df
-
-def search_items_df(df, **kwargs):
-    '''
-    Search rows which have specifies items from a given dataframe.
-    Please pass key words for searching to **kwargs.
-    For example, if you want to get items that is greater than equal (>=)
-    100 in column "A", please specify **kwargs as "A=gte100". Please see below for details.
-    If nothing passed to **kwargs, return input dataframe.
-    Paramters
-    ---------
-        df: DataFrame (pandas)
-            input dataframe
-        **kwargs:
-            key is for column, value is for filtering values (items)
-            You can use indicators below for filtering way.
-            "gt" for ">"
-            "gte" for ">="
-            "lt" for "<"
-            "lte" for "<="
-            "ne" for "!="
-            "c/" for "contains"
-            "" for "=="
-            If you pass tuple to value, this function search and filter items recursively.
-    '''
-    res_df = df
-
-    def f(res_df, k, v):
-        if v == '*':
-            pass
-        elif re.search('^gt\d+', v):
-            v = float(re.search('^gt(\d+\.*\d*)$', v).group(1))
-            res_df = res_df[res_df[k] > v]
-        elif re.search('^gte\d+', v):
-            v = float(re.search('^gte(\d+\.*\d*)$', v).group(1))
-            res_df = res_df[res_df[k] >= v]
-        elif re.search('^lt\d+', v):
-            v = float(re.search('^lt(\d+\.*\d*)$', v).group(1))
-            res_df = res_df[res_df[k] < v]
-        elif re.search('^lte\d+', v):
-            v = float(re.search('lte(\d+\.*\d*)$', v).group(1))
-            res_df = res_df[res_df[k] <= v]
-        elif re.search('^ne\d+', v):
-            v = float(re.search('ne(\d+\.*\d*)$', v).group(1))
-            res_df = res_df[res_df[k] != v]
-        elif re.search('^c\/', v):
-            v = re.search('^c\/(.+)\/$', v).group(1)
-            res_df = res_df[res_df[k].str.contains(v)]
-        else:
-            res_df = res_df[res_df[k] == v]
-        return res_df
-
-    for k,v in kwargs.items():
-        if isinstance(v, tuple):
-            for i in v:
-                res_df = f(res_df, k, i)
-        elif isinstance(v, str):
-            res_df = f(res_df, k, v)
-        elif isinstance(v, int):
-            res_df = res_df[res_df[k] == v]
-
-    return res_df
 
 def adjust_average(data, average=1000, divisor=None, integer=False):
 
