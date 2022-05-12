85813f677965220e0114556c49aa885ab6257c1d
Haruka Yamashita <hyamashita@nig.ac.jp>
2022-05-12 11:24:29 +0900

    Initial commit.

---
+++ dummy.py
@@ -0,0 +1,9 @@
+
+class Alignment:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f'Dummy Alignment class: {self.name}, {self.value}'
+
