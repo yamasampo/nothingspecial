9fa9f6abdd418e072cd53274f71304ddd6df48a8
Haruka Yamashita <hyamashita@nig.ac.jp>
2022-11-25 16:39:40 +0900

    Initial commit.

---
+++ logging.py
@@ -0,0 +1,5 @@
+""" Track functions that are called during a process and output their names and 
+versions. This attempts to make it easier to reproduce an analysis and also to
+reduce time to trace back my codes when publishing.
+"""
+
