0791a49b14af532e07b6737c52d818e1383d83de
Haruka Yamashita <hyamashita@nig.ac.jp>
2022-11-25 14:43:34 +0900

    Move some constants from variables.py

--- constants.py
+++ constants.py
@@ -1,3 +1,36 @@
+
+BASES = ['T', 'C', 'A', 'G']
+
+chr_name_dict_all = {
+    '1': 'YHet',
+    '2': 'dmel_mitochondrion_genome',
+    '3': '2L',
+    '4': 'X',
+    '5': '3L',
+    '6': '4',
+    '7': '2R',
+    '8': '3R',
+    '9': 'Uextra',
+    '10': '2RHet',
+    '11': '2LHet',
+    '12': '3LHet',
+    '13': '3RHet',
+    '14': 'U',
+    '15': 'XHet'
+}
+
+chr_name_dict_auto = {
+    '3': '2L',
+    '5': '3L',
+    '6': '4',
+    '7': '2R',
+    '8': '3R',
+    '10': '2RHet',
+    '11': '2LHet',
+    '12': '3LHet',
+    '13': '3RHet',
+}
+
 TISSUE_TYPES_a = {
     't1': 'Ad hindgut',
     't2': 'Ad midgut',
